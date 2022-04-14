f26c0c3a2f26865fe78e10d13ef8238ffe9aa680
romijulianto <romyjulians@gmail.com>
2022-04-14 15:09:44 +0700

    codelab practice 2

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tempat_wisata/detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: DetailScreen(),
+    );
+  }
+}
